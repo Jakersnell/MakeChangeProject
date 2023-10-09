72887943b19693e6ec4c613755f7c508179d3a9c
Jacob Snell <jake.r.snell@gmail.com>
2023-10-09 08:37:37 -0600

    final push

--- src/makechange/Register.java
+++ src/makechange/Register.java
@@ -3,82 +3,101 @@ package makechange;
 import java.util.Scanner;
 
 public class Register {
+	
+	static Scanner input = new Scanner(System.in);
+	
+	static double roundingOffset = 0.0005;
+	
+	static double[] tenderSizes = { 20.0, 10.0, 5.0, 1.0, 0.25, 0.10, 0.05, 0.01 };
+	
+	static String[] tenderNames = { "Twenty dollar bill", "Ten dollar bill", "Five dollar bill", "One dollar bill",
+			"Quarter", "Dime", "Nickel", "Penny" };
+	
 
 	public static void main(String[] args) {
-		Scanner input = new Scanner(System.in);
 
-		Double[] tenderSizes = { 20.0, 10.0, 5.0, 1.0, 0.25, 0.10, 0.05, 0.01 };
-		String[] tenderNames = { "Twenty dollar bill", "Ten dollar bill", "Five dollar bill", "One dollar bill",
-				"Quarter", "Dime", "Nickel", "Penny", "Pennies" };
+		double cashBack = getExchangeData();
+		
+		if (cashBack < 0) {
 
+			System.err.printf("You provided to little cash to cover the price!\nYou still owe: $%.2f", (-cashBack));
+
+		} else if (cashBack == 0) {
+
+			System.out.printf("You paid the exact amount, you are owed no change! Thank you for your business!");
+
+		} else {
+			
+			makeChange(cashBack);
+			
+		}
+
+		input.close();
+		
+	}
+	
+	
+	public static double getExchangeData() {
 		System.out.print("What is the total of your purchase?\n>>> ");
 		double total = input.nextDouble();
 
 		System.out.print("How much cash will you use to pay for it?\n>>> ");
 		double cashGiven = input.nextDouble();
 
-		double cashToReturn = (double) Math.round((cashGiven - total) * 100) / 100 + 0.005;
-		System.out.println(cashToReturn);
+		double cashBack = (cashGiven - total) + roundingOffset;
+		
+		return cashBack;
+	}
+	
 
-		if (cashToReturn < 0) {
+	public static void makeChange(double cashBack) {
+		
+		int i;
 
-			System.err.printf("You provided to little cash to cover the price!\nYou still owe: $%.2f", (-cashToReturn));
+		boolean isFirstToPrint = true;
 
-		} else if (cashToReturn == 0) {
+		double remainder;
+		double tenderSize;
 
-			System.out.printf("You paid the exact amount, you are owed no change! Thank you for your business!");
+		int amountOfCurrentSize;
 
-		} else {
-			int amountOfCurrentSize;
-			double remainder;
-			double billSize;
+		String tenderName;
+		String outPut = "";
 
-			boolean isMultiple = false;
-			boolean isFirstToPrint = true;
-			boolean isPenny = false;
+		System.out.println("You are owed: ");
+		for (i = 0; i < tenderSizes.length; i++) {
 
-			String addOn;
+			tenderSize = tenderSizes[i];
 
-			System.out.println("You are owed: ");
-			for (int i = 0; i < tenderSizes.length; i++) {
-				addOn = "";
+			amountOfCurrentSize = (int) (cashBack / tenderSize);
 
-				billSize = tenderSizes[i];
+			if (amountOfCurrentSize != 0) {
+				tenderName = tenderNames[i];
 
-				amountOfCurrentSize = (int) (cashToReturn / billSize);
+				remainder = cashBack % tenderSize;
+				cashBack = remainder;
 
-				remainder = cashToReturn % billSize;
-				cashToReturn = remainder;
-
-				isMultiple = (amountOfCurrentSize > 1.0);
-				isPenny = (i == tenderSizes.length - 1);
-
-				if (isMultiple) {
-					if (isPenny) {
-						i++;
+				if (amountOfCurrentSize > 1.0) {
+					if (tenderName.equals("Penny")) {
+						tenderName = "Pennies";
 					} else {
-						addOn = "s";
+						tenderName += "s";
 					}
 				}
 
-				if (amountOfCurrentSize != 0) {
-
-					if (!isFirstToPrint) {
-						System.out.printf(", ");
-					} else {
-						isFirstToPrint = false;
-					}
-
-					System.out.printf("%d %s", amountOfCurrentSize, tenderNames[i] + addOn);
-
+				if (!isFirstToPrint) {
+					System.out.printf(", ");
+				} else {
+					isFirstToPrint = false;
 				}
+
+				System.out.printf("%d %s", amountOfCurrentSize, tenderName);
 
 			}
 
-			System.out.println();
-
 		}
 
-		input.close();
+		System.out.println();
+		
 	}
 }
